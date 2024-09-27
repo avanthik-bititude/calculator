503945c0c2631a95603bb131a82943f34b494b1a
avanthik <avanthik@bititude.com>
2024-09-27 11:38:22 +0530

    completed calculator

---
+++ script.js
@@ -0,0 +1,93 @@
+let operand1 = "";
+let operand2 = "";
+let curOperator = null;
+let result = null;
+let isNewCal = false;
+let display = document.getElementById("result-number");
+console.log("display", display);
+
+let numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (isNewCal) {
+      operand1 = "";
+      isNewCal = false;
+    }
+    if (!curOperator) {
+      operand1 += number.getAttribute("data-value");
+    } else {
+      operand2 += number.getAttribute("data-value");
+    }
+    if (operand1) {
+      display.innerText = operand1.toString();
+    }
+    if (curOperator) {
+      display.innerText += curOperator + operand2.toString();
+    }
+  });
+});
+
+document.querySelectorAll(".operator").forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (operand1 && curOperator && operand2) {
+      operate();
+    }
+    curOperator = operator.getAttribute("data-value");
+    isNewCal = false;
+  });
+});
+
+document.querySelectorAll(".clear").forEach((button) => {
+  button.addEventListener("click", () => {
+    operand1 = "";
+    operand2 = "";
+    curOperator = null;
+    result = null;
+    display.innerText = "0";
+  });
+});
+
+document.querySelectorAll(".equals").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (operand1 && curOperator && operand2) {
+      operate();
+      isNewCal = true;
+    } else {
+      console.log("somthing missing at equals operations error");
+    }
+  });
+});
+
+function operate() {
+  console.log(operand1, curOperator, operand2);
+
+  let num1 = parseFloat(operand1);
+  let num2 = parseFloat(operand2);
+  console.log(num1, num2);
+
+  switch (curOperator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 === 0) {
+        display.innerText = "cant / by 0";
+        return
+      } else {
+        result = num1 / num2;
+      }
+    default:
+      display.innerHTML = "somthing error";
+  }
+  result = Math.round(result * 100) / 100;
+  display.innerHTML = result;
+  operand1 = result.toString();
+  operand2 = "";
+  curOperator = null;
+}
